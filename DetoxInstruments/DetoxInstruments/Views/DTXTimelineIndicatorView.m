0ff650ac0fb72e8758358bcfba2bd81f9f00100a
Leo Natan <leo.natan@outlook.com>
2017-08-02 10:51:43 +0300

    Display timeline indicator view.

--- DetoxInstruments/DetoxInstruments/Views/DTXTimelineIndicatorView.m
+++ DetoxInstruments/DetoxInstruments/Views/DTXTimelineIndicatorView.m
@@ -26,41 +26,8 @@
 
 @end
 
-@interface _DTXDashedLine : NSView <CALayerDelegate>
-@end
-
-@implementation _DTXDashedLine
-
-- (CALayer *)makeBackingLayer
-{
-	return [_DTXDashedLineLayer new];
-}
-
--(void)viewDidChangeBackingProperties
-{
-	if (self.window)
-	{
-		self.layer.contentsScale = self.window.backingScaleFactor;
-	}
-	else
-	{
-		self.layer.contentsScale = 1.0;
-	}
-	
-	[self setNeedsDisplay:YES];
-}
-
-- (void)setFrame:(NSRect)frame
-{
-	[super setFrame:frame];
-}
-
-@end
-
+@interface DTXTimelineIndicatorView () <CALayerDelegate> @end
 @implementation DTXTimelineIndicatorView
-{
-	_DTXDashedLine* _dashedLine;
-}
 
 - (instancetype)initWithFrame:(NSRect)frameRect
 {
@@ -69,11 +36,6 @@
 	if(self)
 	{
 		self.wantsLayer = YES;
-		_dashedLine = [[_DTXDashedLine alloc] initWithFrame:NSMakeRect(0, 0, 1, frameRect.size.height)];
-		_dashedLine.hidden = YES;
-		_dashedLine.wantsLayer = YES;
-		_dashedLine.layer.backgroundColor = [NSColor.blackColor colorWithAlphaComponent:0.45].CGColor;
-		[self addSubview:_dashedLine];
 		self.acceptsTouchEvents = NO;
 	}
 	
@@ -91,7 +53,7 @@
 		self.layer.contentsScale = 1.0;
 	}
 	
-	_dashedLine.frame = (NSRect){floor(_indicatorOffset), _dashedLine.frame.origin.y, 1 / self.layer.contentsScale, self.bounds.size.height};
+	[self.layer setNeedsDisplay];
 }
 
 - (BOOL)acceptsFirstResponder
@@ -99,18 +61,11 @@
 	return NO;
 }
 
-//- (BOOL)isFlipped
-//{
-//	return YES;
-//}
-
 - (void)layout
 {
 	[super layout];
 	
-	CGRect frame = _dashedLine.frame;
-	frame.size.height = self.bounds.size.height;
-	_dashedLine.frame = frame;
+	[self.layer setNeedsDisplay];
 }
 
 - (void)viewDidMoveToWindow
@@ -126,21 +81,36 @@
 - (void)setIndicatorOffset:(CGFloat)indicatorOffset
 {
 	_indicatorOffset = indicatorOffset;
-	//Causes bug in High Sierra
-	if(NSProcessInfo.processInfo.operatingSystemVersion.minorVersion <= 12)
-	{
-		_dashedLine.frame = (NSRect){floor(indicatorOffset), _dashedLine.frame.origin.y, _dashedLine.frame.size};
-	}
+	
+	[self.layer setNeedsDisplay];
 }
 
 - (void)setDisplaysIndicator:(BOOL)displaysIndicator
 {
 	_displaysIndicator = displaysIndicator;
-	//Causes bug in High Sierra
-	if(NSProcessInfo.processInfo.operatingSystemVersion.minorVersion <= 12)
+	
+	[self.layer setNeedsDisplay];
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	
+}
+
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)context
+{
+	if(_displaysIndicator == NO)
 	{
-		_dashedLine.hidden = _displaysIndicator == NO;
+		return;
 	}
+	
+	CGContextSetLineWidth(context, 1.5 / self.layer.contentsScale);
+	CGContextSetLineDash(context, 5.0, (CGFloat[]){5.,5.}, 2);
+	
+	CGContextSetStrokeColorWithColor(context, [NSColor.blackColor colorWithAlphaComponent:0.65].CGColor);
+	CGContextMoveToPoint(context, _indicatorOffset, 0);    // This sets up the start point
+	CGContextAddLineToPoint(context, _indicatorOffset, self.bounds.size.height);
+	CGContextStrokePath(context);
 }
 
 @end
