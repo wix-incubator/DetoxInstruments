f49e6f55edb91145a617d9581b447f786b1c1ebf
Leo Natan <lnatan@wix.com>
2017-08-22 21:13:37 +0300

    Display device preview with actual device color bezels
    Display simulator devices as such
    Revert options button placement in Detox Instruments

--- Profiler/DTXProfiler/Utilities/DTXDeviceInfo.m
+++ Profiler/DTXProfiler/Utilities/DTXDeviceInfo.m
@@ -10,8 +10,18 @@
 #import "DBBuildInfoProvider.h"
 #import "DTXReactNativeSampler.h"
 @import UIKit;
+@import Darwin;
 
 @implementation DTXDeviceInfo
+
++ (NSString*)_machineName
+{
+	struct utsname systemInfo;
+	
+	uname(&systemInfo);
+	
+	return [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+}
 
 + (NSDictionary*)deviceInfoDictionary
 {
@@ -22,7 +32,13 @@
 	NSMutableDictionary* deviceDetails = [NSMutableDictionary new];
 	deviceDetails[@"appName"] = buildProvider.applicationDisplayName;
 	deviceDetails[@"binaryName"] = processInfo.processName;
+#if ! TARGET_OS_SIMULATOR
 	deviceDetails[@"deviceName"] = currentDevice.name;
+#else
+	NSString* deviceTypeSim = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ? @"iPad" : @"iPhone";
+	
+	deviceDetails[@"deviceName"] = [NSString stringWithFormat:NSLocalizedString(@"%@ Simulator (%@)", @""), deviceTypeSim, currentDevice.name];
+#endif
 	deviceDetails[@"deviceOS"] = processInfo.operatingSystemVersionString;
 	deviceDetails[@"deviceOSType"] = 0;
 	deviceDetails[@"devicePhysicalMemory"] = @(processInfo.physicalMemory);
@@ -30,6 +46,30 @@
 	deviceDetails[@"deviceType"] = currentDevice.model;
 	deviceDetails[@"processIdentifier"] = @(processInfo.processIdentifier);
 	deviceDetails[@"hasReactNative"] = @([DTXReactNativeSampler reactNativeInstalled]);
+	
+#if ! TARGET_OS_SIMULATOR
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+	SEL selector = NSSelectorFromString(@"deviceInfoForKey:");
+	
+	if (![currentDevice respondsToSelector:selector])
+	{
+		selector = NSSelectorFromString(@"_deviceInfoForKey:");
+	}
+	
+	if ([currentDevice respondsToSelector:selector])
+	{
+		deviceDetails[@"deviceColor"] = [currentDevice performSelector:selector withObject:@"DeviceColor"];
+		deviceDetails[@"deviceEnclosureColor"] = [currentDevice performSelector:selector withObject:@"DeviceEnclosureColor"];
+	}
+#pragma clang diagnostic pop
+	
+	deviceDetails[@"machineName"] = self._machineName;
+#else
+	deviceDetails[@"deviceColor"] = @"1";
+	deviceDetails[@"deviceEnclosureColor"] = @"1";
+	deviceDetails[@"machineName"] = deviceTypeSim;
+#endif
 	
 	return deviceDetails;
 }
