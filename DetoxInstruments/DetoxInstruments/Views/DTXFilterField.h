bf748ce646e6867dd45e03247dc8902e3b6fa065
Artal Druk <artald@wix.com>
2017-08-28 10:53:30 +0300

    Add UI for filtering bottom content

---
+++ DetoxInstruments/DetoxInstruments/Views/DTXFilterField.h
@@ -0,0 +1,13 @@
+//
+//  DTXFilterField.h
+//  DetoxInstruments
+//
+//  Created by Artal Druk on 28/08/2017.
+//  Copyright © 2017 Wix. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+IB_DESIGNABLE
+@interface DTXFilterField : NSSearchField <NSSearchFieldDelegate>
+@end
---
+++ Shared/Images/ImageGenerator.m
@@ -0,0 +1,101 @@
+//
+//  ImageGenerator.m
+//  DetoxInstruments
+//
+//  Created by Artal Druk on 27/08/2017.
+//  Copyright © 2017 Wix. All rights reserved.
+//
+
+#import "ImageGenerator.h"
+
+typedef void (^DrawingBlock)(CGContextRef context);
+
+@implementation ImageGenerator
+
++ (NSImage*) drawGraphicsWithPixelsWidth:(int)width pixelsHight:(int)height drawingBlock:(DrawingBlock)drawingBlock
+{
+	CGFloat scale = [[NSScreen mainScreen] backingScaleFactor];
+	CGSize imageSize = NSMakeSize(width, height);
+	
+	NSBitmapImageRep *bmpImageRep = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL
+									 pixelsWide:width * scale pixelsHigh:height * scale
+									 bitsPerSample:8 samplesPerPixel:4
+									 hasAlpha:YES isPlanar:NO
+									 colorSpaceName:NSCalibratedRGBColorSpace bitmapFormat:NSAlphaFirstBitmapFormat
+									 bytesPerRow:0 bitsPerPixel:0];
+	[bmpImageRep setSize:imageSize];
+	
+	NSGraphicsContext *bitmapContext = [NSGraphicsContext graphicsContextWithBitmapImageRep:bmpImageRep];
+	[NSGraphicsContext saveGraphicsState];
+	[NSGraphicsContext setCurrentContext:[NSGraphicsContext graphicsContextWithBitmapImageRep:bmpImageRep]];
+	
+	drawingBlock(bitmapContext.CGContext);
+	
+	[NSGraphicsContext restoreGraphicsState];
+	
+	CGImageRef cgImage = CGBitmapContextCreateImage(bitmapContext.CGContext);
+	NSImage *nsImage = [[NSImage alloc] initWithCGImage:cgImage size:imageSize];
+	CGImageRelease(cgImage);
+	return nsImage;
+}
+
++ (void) drawHorizontalLineInContext:(CGContextRef)context iconsSize:(CGFloat)iconsSize offset:(CGPoint)offset
+{
+	CGContextMoveToPoint(context, offset.x, iconsSize - offset.y);
+	CGContextAddLineToPoint(context, iconsSize - offset.x, iconsSize - offset.y);
+}
+
++ (NSImage*)_createFilterImageWithSize:(int)filterIconSize highlighted:(BOOL)highlighted
+{
+	return [self drawGraphicsWithPixelsWidth:filterIconSize pixelsHight:filterIconSize drawingBlock:^(CGContextRef context){
+		CGFloat lineWidth = highlighted ? 1.5 : 1.0;
+		CGContextSetLineWidth(context, lineWidth);
+		highlighted ? CGContextSetRGBStrokeColor(context, 0.09, 0.49, 0.949, 1) : CGContextSetRGBStrokeColor(context, 0.482, 0.482, 0.482, 1);
+		CGContextStrokeEllipseInRect(context, CGRectMake (lineWidth, lineWidth, filterIconSize - lineWidth * 2, filterIconSize - lineWidth * 2));
+		
+		CGContextBeginPath(context);
+		{
+			CGContextSetLineWidth(context, 1.0);
+			[self drawHorizontalLineInContext:context iconsSize:filterIconSize offset:CGPointMake(3.5, 5)];
+			[self drawHorizontalLineInContext:context iconsSize:filterIconSize offset:CGPointMake(4.5, 7)];
+			[self drawHorizontalLineInContext:context iconsSize:filterIconSize offset:CGPointMake(5.5, 9)];
+		}
+		CGContextStrokePath(context);
+	}];
+}
+
++ (NSImage*)createCancelImageWithSize:(int)cancelIconSize
+{
+	return [self drawGraphicsWithPixelsWidth:cancelIconSize pixelsHight:cancelIconSize drawingBlock:^(CGContextRef context){
+		CGFloat padding = 0.3 * cancelIconSize;
+		NSSize imageSize = NSMakeSize(cancelIconSize, cancelIconSize);
+		
+		CGContextSetRGBFillColor(context, 0.482, 0.482, 0.482, 1);
+		CGContextFillEllipseInRect(context, CGRectMake (0, 0, imageSize.width, imageSize.height));
+		
+		CGContextSetRGBStrokeColor(context, 1, 1, 1, 1);
+		CGContextBeginPath(context);
+		CGContextMoveToPoint(context, padding, padding);
+		CGContextAddLineToPoint(context, cancelIconSize - padding, cancelIconSize - padding);
+		CGContextMoveToPoint(context, padding, cancelIconSize - padding);
+		CGContextAddLineToPoint(context, cancelIconSize - padding, padding);
+		CGContextStrokePath(context);
+	}];
+}
+
++ (NSImage*)createFilterImageWithSize:(int)filterIconSize highlighted:(BOOL)highlighted
+{
+	static NSImage* filterColorImage = nil;
+	static NSImage* highLightedFilterColorImage = nil;
+	if(highlighted && highLightedFilterColorImage == nil)
+	{
+		highLightedFilterColorImage = [self _createFilterImageWithSize:filterIconSize highlighted:YES];
+	}
+	else if(!highlighted && filterColorImage == nil)
+	{
+		filterColorImage = [self _createFilterImageWithSize:filterIconSize highlighted:NO];
+	}
+	return highlighted ? highLightedFilterColorImage : filterColorImage;
+}
+
+@end
