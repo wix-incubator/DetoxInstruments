98e31e8b180e425b60a163386b24f7c8329d7312
Leo Natan <lnatan@wix.com>
2017-11-07 12:05:37 +0200

    Make profiling configuration immutable
    Add mutable variant
    Support copy and mutableCopy

--- Profiler/DTXProfiler/DTXProfilingConfiguration.h
+++ Profiler/DTXProfiler/DTXProfilingConfiguration.h
@@ -13,7 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  *  Profiling configuration object for the Profiler.
  */
-@interface DTXProfilingConfiguration : NSObject
+@interface DTXProfilingConfiguration : NSObject <NSCopying, NSMutableCopying>
 
 /**
  *  Returns a newly created default profiling configuration object.
@@ -32,7 +32,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  The default value is 0.5.
  */
-@property (nonatomic) NSTimeInterval samplingInterval;
+@property (nonatomic, readonly) NSTimeInterval samplingInterval;
 
 /**
  *  The minimum number of samples to keep in memory before flushing to disk.
@@ -41,7 +41,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  The default value is @c 200.
  */
-@property (nonatomic) NSUInteger numberOfSamplesBeforeFlushToDisk;
+@property (nonatomic, readonly) NSUInteger numberOfSamplesBeforeFlushToDisk;
 
 //Recording Configuration
 
@@ -50,7 +50,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  The default value is @c true.
  */
-@property (nonatomic) BOOL recordNetwork;
+@property (nonatomic, readonly) BOOL recordNetwork;
 
 /**
  *  Record localhost network requests during profiling.
@@ -59,14 +59,14 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  The default value is @c false.
  */
-@property (nonatomic) BOOL recordLocalhostNetwork;
+@property (nonatomic, readonly) BOOL recordLocalhostNetwork;
 
 /**
  *  Record thread information during profiling.
  *
  *  The default value is @c true.
  */
-@property (nonatomic) BOOL recordThreadInformation;
+@property (nonatomic, readonly) BOOL recordThreadInformation;
 
 /**
  *  Collect stack trace information where appropriate.
@@ -75,7 +75,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  The default value is @c false.
  */
-@property (nonatomic) BOOL collectStackTraces;
+@property (nonatomic, readonly) BOOL collectStackTraces;
 
 /**
  *  Symbolicate stack traces at runtime.
@@ -84,14 +84,14 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  The default value is @c false.
  */
-@property (nonatomic) BOOL symbolicateStackTraces;
+@property (nonatomic, readonly) BOOL symbolicateStackTraces;
 
 /**
  *  Record log output during profiling.
  *
  *  The default value is @c true.
  */
-@property (nonatomic) BOOL recordLogOutput;
+@property (nonatomic, readonly) BOOL recordLogOutput;
 
 /**
  *  Profile React Native application.
@@ -101,7 +101,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  The default value is @c true.
  */
-@property (nonatomic) BOOL profileReactNative;
+@property (nonatomic, readonly) BOOL profileReactNative;
 
 /**
  *  Collect JavaScript stack trace information where appropriate.
@@ -113,7 +113,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  The default value is @c false.
  */
-@property (nonatomic) BOOL collectJavaScriptStackTraces;
+@property (nonatomic, readonly) BOOL collectJavaScriptStackTraces;
 
 /**
  *  Symbolicate JavaScript stack traces at runtime using source maps (if available).
@@ -122,7 +122,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  The default value is @c false.
  */
-@property (nonatomic) BOOL symbolicateJavaScriptStackTraces;
+@property (nonatomic, readonly) BOOL symbolicateJavaScriptStackTraces;
 
 /* Output Configuration */
 
@@ -131,7 +131,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  The default value is @c false.
  */
-@property (nonatomic) BOOL prettyPrintJSONOutput;
+@property (nonatomic, readonly) BOOL prettyPrintJSONOutput;
 
 /**
  *  The recording file URL to save to.
@@ -146,7 +146,137 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  The default value is a file name with the date and time of the recording, in the documents folder of the device.
  */
-@property (nonatomic, copy, null_resettable) NSURL* recordingFileURL;
+@property (nonatomic, copy, null_resettable, readonly) NSURL* recordingFileURL;
+
+@end
+
+#pragma mark -
+
+/**
+ *  Profiling configuration object for the Profiler.
+ */
+@interface DTXMutableProfilingConfiguration : DTXProfilingConfiguration
+
+/**
+ *  The sampling interval of the Profiler.
+ *
+ *  The default value is 0.5.
+ */
+@property (nonatomic, readwrite) NSTimeInterval samplingInterval;
+
+/**
+ *  The minimum number of samples to keep in memory before flushing to disk.
+ *
+ *  Larger number of samples in memory will improve performance at the cost of memory use.
+ *
+ *  The default value is @c 200.
+ */
+@property (nonatomic, readwrite) NSUInteger numberOfSamplesBeforeFlushToDisk;
+
+//Recording Configuration
+
+/**
+ *  Record network requests during profiling.
+ *
+ *  The default value is @c true.
+ */
+@property (nonatomic, readwrite) BOOL recordNetwork;
+
+/**
+ *  Record localhost network requests during profiling.
+ *
+ *  Only relevant if @c recordNetwork is set to @c true.
+ *
+ *  The default value is @c false.
+ */
+@property (nonatomic, readwrite) BOOL recordLocalhostNetwork;
+
+/**
+ *  Record thread information during profiling.
+ *
+ *  The default value is @c true.
+ */
+@property (nonatomic, readwrite) BOOL recordThreadInformation;
+
+/**
+ *  Collect stack trace information where appropriate.
+ *
+ *  Collecting stack traces may introduce some performance hit.
+ *
+ *  The default value is @c false.
+ */
+@property (nonatomic, readwrite) BOOL collectStackTraces;
+
+/**
+ *  Symbolicate stack traces at runtime.
+ *
+ *  Symbolicating stack traces may introduce some performance hit.
+ *
+ *  The default value is @c false.
+ */
+@property (nonatomic, readwrite) BOOL symbolicateStackTraces;
+
+/**
+ *  Record log output during profiling.
+ *
+ *  The default value is @c true.
+ */
+@property (nonatomic, readwrite) BOOL recordLogOutput;
+
+/**
+ *  Profile React Native application.
+ *
+ *  Currently, only one active React Native bridge is supported.
+ *  If you have different needs, open an issue at @ref https://github.com/wix/DetoxInstruments/issues
+ *
+ *  The default value is @c true.
+ */
+@property (nonatomic, readwrite) BOOL profileReactNative;
+
+/**
+ *  Collect JavaScript stack trace information where appropriate.
+ *
+ *  Collecting JavaScript stack traces may introduce some performance hit.
+ *
+ *  JavaScript stack trace collection is implemented using signals. Specifically, @c SIGPROF and @c SIGCHLD are used.
+ *  If your application uses those signals or installs custom handlers for those signals, there may be collision issues.
+ *
+ *  The default value is @c false.
+ */
+@property (nonatomic, readwrite) BOOL collectJavaScriptStackTraces;
+
+/**
+ *  Symbolicate JavaScript stack traces at runtime using source maps (if available).
+ *
+ *  Symbolicating JavaScript stack traces may introduce some performance hit.
+ *
+ *  The default value is @c false.
+ */
+@property (nonatomic, readonly) BOOL symbolicateJavaScriptStackTraces;
+
+/* Output Configuration */
+
+/**
+ *  Prints the JSON portion of the output in a pretty manner.
+ *
+ *  The default value is @c false.
+ */
+@property (nonatomic, readwrite) BOOL prettyPrintJSONOutput;
+
+/**
+ *  The recording file URL to save to.
+ *
+ *  If this URL is a directory URL, a new recording will be created in that directory with the date and time of the recording.
+ *
+ *  If the URL is a file URL, a new recording will be created with that name.
+ *
+ *  The extension of the recording package is always @c .dtxprof.
+ *
+ *  If set to @c nil, the value will reset to the default value.
+ *
+ *  The default value is a file name with the date and time of the recording, in the documents folder of the device.
+ */
+@property (nonatomic, copy, null_resettable, readwrite) NSURL* recordingFileURL;
 
 @end
 
