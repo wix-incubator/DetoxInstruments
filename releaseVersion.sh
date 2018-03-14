c874158cfbba10e85ad1180ec7cbf2e6cc71a65c
Leo Natan <leo.natan@outlook.com>
2018-03-14 12:42:13 +0200

    Add some color to the release script

--- releaseVersion.sh
+++ releaseVersion.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -e
 
 if  [[ -n $(git status --porcelain) ]]; then
-	printf >&2 "Cannot release version because there are unstaged changes, aborting.\nChanges:"
+	printf >&2 "\033[1;31mCannot release version because there are unstaged changes, aborting.\nChanges:\033[0m\n"
 	git status --short
 	exit -1
 fi
 
 if [[ -n $(git log --branches --not --remotes) ]]; then
-	echo "Pushing pending commits to git"
+	echo -e "\033[1;34mPushing pending commits to git\033[0m"
 	git push &> /dev/null
 fi
 
-echo "Creating release notes"
+echo -e "\033[1;34mCreating release notes\033[0m"
 
 RELEASE_NOTES_FILE=Distribution/_tmp_release_notes.md
 
@@ -20,12 +20,12 @@ touch "${RELEASE_NOTES_FILE}"
 open -Wn "${RELEASE_NOTES_FILE}"
 
 if ! [ -s "${RELEASE_NOTES_FILE}" ]; then
-  echo >&2 "No release notes provided, aborting."
+  echo >&2 "\033[1;31mNo release notes provided, aborting.\033[0m"
   rm -f "${RELEASE_NOTES_FILE}"
   exit -1
 fi
 
-echo "Building archive and exporting"
+echo -e "\033[1;34mBuilding archive and exporting\033[0m"
 
 ARCHIVE=Distribution/Archive.xcarchive
 EXPORT_DIR=Distribution/Export
@@ -42,7 +42,7 @@ BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${EXPORT_DIR}
 VERSION="${SHORT_VERSION}"."${BUILD_NUMBER}"
 ZIP_FILE=Distribution/DetoxInstruments-v"${SHORT_VERSION}".b"${BUILD_NUMBER}".zip
 
-echo "Updating cask with latest release"
+echo -e "\033[1;34mUpdating cask with latest release\033[0m"
 
 pushd . &> /dev/null
 cd Distribution/homebrew-brew/Casks/
@@ -54,38 +54,38 @@ git commit -m "Detox Instruments ${VERSION}" &> /dev/null
 git push &> /dev/null
 popd &> /dev/null
 
-echo "Pushing updated versions"
+echo -e "\033[1;34mPushing updated versions\033[0m"
 
 git add -A &> /dev/null
 git commit -m "${VERSION}" &> /dev/null
 git push &> /dev/null
 
-echo "Creating ZIP file"
+echo -e "\033[1;34mCreating ZIP file\033[0m"
 
 ditto -c -k --sequesterRsrc --keepParent "${EXPORT_DIR}"/*.app "${ZIP_FILE}" &> /dev/null
 
 #Escape user input in markdown to valid JSON string using PHP 🤦‍♂️ (https://stackoverflow.com/a/13466143/983912)
 RELEASENOTESCONTENTS=$(printf '%s' "$(<"${RELEASE_NOTES_FILE}")" | php -r 'echo json_encode(file_get_contents("php://stdin"));')
 
-echo "Creating a GitHub release"
+echo -e "\033[1;34mCreating a GitHub release\033[0m"
 
 API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master", "name": "v%s", "body": %s, "draft": false, "prerelease": false}' "$VERSION" "$VERSION" "$RELEASENOTESCONTENTS")
 RELEASE_ID=$(curl -s --data "$API_JSON" https://api.github.com/repos/wix/DetoxInstruments/releases?access_token=${GITHUB_RELEASES_TOKEN} | jq ".id")
 
-echo "Uploading ZIP attachment to release"
+echo -e "\033[1;34mUploading ZIP attachment to release\033[0m"
 
 curl -s --data-binary @"${ZIP_FILE}" -H "Content-Type: application/octet-stream" "https://uploads.github.com/repos/wix/DetoxInstruments/releases/${RELEASE_ID}/assets?name=$(basename ${ZIP_FILE})&access_token=${GITHUB_RELEASES_TOKEN}" | jq "." &> /dev/null
 
-echo "Triggering gh-pages rebuild"
+echo -e "\033[1;34mTriggering gh-pages rebuild\033[0m"
 
 curl -H "Content-Type: application/json; charset=UTF-8" -X PUT -d '{"message": "Rebuild GH Pages", "committer": { "name": "PublishScript", "email": "somefakeaddress@wix.com" }, "content": "LnB1Ymxpc2gK", "sha": "3f949857e8ed4cb106f9744e40b638a7aabf647f", "branch": "gh-pages"}' https://api.github.com/repos/wix/DetoxInstruments/contents/.publish?access_token=${GITHUB_RELEASES_TOKEN}
 
-echo "Opening archive in Xcode"
+echo -e "\033[1;34mOpening archive in Xcode\033[0m"
 
 open "${ARCHIVE}"
-sleep 1
+sleep 15
 
-echo "Cleaning up"
+echo -e "\033[1;34mCleaning up\033[0m"
 
 rm -f "${RELEASE_NOTES_FILE}"
 rm -f "${ZIP_FILE}"
