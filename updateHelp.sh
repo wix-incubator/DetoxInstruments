913964f7b28167731dd95dc44a7ee261def85600
Leo Natan <leo.natan@outlook.com>
2018-04-25 14:06:35 +0300

    Update help style

--- updateHelp.sh
+++ updateHelp.sh
@@ -5,6 +5,7 @@ HTMLDIR=DetoxInstruments/DetoxInstruments.help/Contents/Resources/English.lproj
 rm -fr "${HTMLDIR}"/Documentation
 mkdir -p "${HTMLDIR}"/Documentation
 cp -R Documentation/Resources "${HTMLDIR}"/Documentation/
+cp DetoxInstruments/DetoxInstruments/DetoxInstruments.iconset/icon_512x512@2x.png "${HTMLDIR}"/Documentation/Resources/
 
 function render_markdown {
   SOURCE_FILE="$1"
@@ -19,6 +20,7 @@ function render_markdown {
   # curl -s 'https://raw.githubusercontent.com/sindresorhus/github-markdown-css/gh-pages/github-markdown.css' >> "${TARGET_FILE}"
   echo '</style>' >> "${TARGET_FILE}"
   echo '<style type="text/css">body { font-family: -apple-system-font, -webkit-system-font, "HelveticaNeue", "Helvetica Neue", "Helvetica", sans-serif; font-size: 13px; padding: 0px 10px 20px 10px; }'  >> "${TARGET_FILE}"
+  echo 'h1 { text-align: center; margin-left: -16px; margin-right: -16px; padding-bottom: 20px; background: linear-gradient(to bottom, #ffffff 0%,#f3f2f3 100%); }' >> "${TARGET_FILE}"
   echo 'img { max-width: 100%; height: auto; }'  >> "${TARGET_FILE}"
   echo 'pre { background-color: rgb(246, 248, 250); border-bottom-left-radius: 3px; border-bottom-right-radius: 3px; border-top-left-radius: 3px; border-top-right-radius: 3px; box-sizing: border-box; color: rgb(36, 41, 46); display: block; font-family: SFMono-Regular, Consolas, "Liberation Mono", Menlo, Courier, monospace; font-size: 13.600000381469727px; line-height: 19px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; margin-top: 0px; overflow-x: auto; overflow-y: auto; padding-bottom: 16px; padding-left: 16px; padding-right: 16px; padding-top: 16px; white-space: pre; word-break: normal; word-wrap: normal; }'  >> "${TARGET_FILE}"
   echo 'blockquote { border-left-color: rgb(245, 242, 240); border-left-style: solid; border-left-width: 4px; box-sizing: border-box; color: rgb(106, 115, 125); display: block; margin-bottom: 16px; margin-left: 0px; margin-right: 0px; margin-top: 0px; padding-bottom: 0px; padding-left: 16px; padding-right: 16px; padding-top: 0px; word-wrap: break-word; }' >> "${TARGET_FILE}"
@@ -32,13 +34,17 @@ function render_markdown {
   
   echo '</body></html>' >> "${TARGET_FILE}"
 
-  #sed doesn't support non-greedy lookup
+  #remove "aria" attributes; sed doesn't support non-greedy lookup
   sed -E -i '' -e 's/\ ?aria-hidden=\"true\"//g' "${TARGET_FILE}"
   sed -E -i '' -e 's/\ ?aria-hidden=\"false\"//g' "${TARGET_FILE}"
   #broken image tags
   sed -E -i '' -e 's/\<img(.*)>\<\/a\>/\<img\1 \/\>\<\/a\>/g' "${TARGET_FILE}"
   #anchor names
   sed -E -i '' -e 's/\<a id/\<a name/g' "${TARGET_FILE}"
+  #add image to top h1
+  if [ $SOURCE_FILE = "README.md" ]; then
+    sed -E -i '' -e 's/\<h1\>/\<h1\>\<img src\=\"Documentation\/Resources\/icon_512x512\@2x\.png\" \alt\=\"Detox Instruments\" title\=\"Detox Instruments\" style\=\"max\-width\:120px\; padding\-bottom\:10px\" \/\>\<br \/\>/' "${TARGET_FILE}"
+  fi
   
   sed -i '' 's/\.md/\.html/' "${TARGET_FILE}"
 }
