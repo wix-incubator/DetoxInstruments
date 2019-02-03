3745ed955d471eec276ebe9cb3be68f859ba0dfb
Leo Natan <leo.natan@outlook.com>
2019-02-03 16:13:03 +0200

    Document type fixes
    Bump to 1.9

---
+++ DetoxInstruments/DetoxInstruments/Document/DTXLegacyRecordingDocument.h
@@ -0,0 +1,17 @@
+//
+//  DTXLegacyRecordingDocument.h
+//  DetoxInstruments
+//
+//  Created by Leo Natan (Wix) on 2/3/19.
+//  Copyright © 2019 Wix. All rights reserved.
+//
+
+#import "DTXRecordingDocument.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTXLegacyRecordingDocument : DTXRecordingDocument
+
+@end
+
+NS_ASSUME_NONNULL_END
