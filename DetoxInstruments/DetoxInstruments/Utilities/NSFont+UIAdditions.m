f85e723c2f0f5da2f68b316f03bf60b64153a041
Leo Natan <leo.natan@outlook.com>
2019-06-28 02:10:27 +0300

    Fix Xcode 10 compilation

--- DetoxInstruments/DetoxInstruments/Utilities/NSFont+UIAdditions.m
+++ DetoxInstruments/DetoxInstruments/Utilities/NSFont+UIAdditions.m
@@ -12,19 +12,23 @@
 
 + (NSFont *)dtx_monospacedSystemFontOfSize:(CGFloat)fontSize weight:(NSFontWeight)weight
 {
+#if __MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_14_4
 	if (@available(macOS 10.15, *))
 	{
 		return [self monospacedSystemFontOfSize:fontSize weight:weight];
 	}
 	else
 	{
+#endif
 		if(weight == NSFontWeightRegular)
 		{
 			return [NSFont userFixedPitchFontOfSize:fontSize];
 		}
 		
 		return [NSFontManager.sharedFontManager fontWithFamily:@"Menlo" traits:NSFixedPitchFontMask | (weight > NSFontWeightRegular ? NSBoldFontMask : 0) weight:0 size:fontSize];
+#if __MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_14_4
 	}
+#endif
 }
 
 @end
