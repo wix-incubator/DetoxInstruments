fa3c933ea2b89df79c88dbff46bd228e233c0f61
Leo Natan <leo.natan@outlook.com>
2019-06-28 02:20:39 +0300

    1.11.10666

--- DetoxInstruments/DetoxInstruments/Preferences/DTXDisplayPreferencesViewController.h
+++ DetoxInstruments/DetoxInstruments/Preferences/DTXDisplayPreferencesViewController.h
@@ -3,7 +3,7 @@
 //  DetoxInstruments
 //
 //  Created by Leo Natan (Wix) on 6/11/19.
-//  Copyright © 2019 Wix. All rights reserved.
+//  Copyright © 2017-2019 Wix. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
--- DetoxInstruments/Thumbnails/ThumbnailProvider.m
+++ DetoxInstruments/Thumbnails/ThumbnailProvider.m
@@ -3,7 +3,7 @@
 //  Thumbnails
 //
 //  Created by Leo Natan (Wix) on 6/10/19.
-//  Copyright © 2019 Wix. All rights reserved.
+//  Copyright © 2017-2019 Wix. All rights reserved.
 //
 
 #import "ThumbnailProvider.h"
