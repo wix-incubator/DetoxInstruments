db4fb353be5bbe2d3070642cc153f03612b0ff57
Leo Natan <leo.natan@outlook.com>
2019-09-09 07:41:42 +0300

    1.11.10815

--- DetoxInstruments/DetoxInstruments/Controllers/DTXExpandedPreviewWindowController.m
+++ DetoxInstruments/DetoxInstruments/Controllers/DTXExpandedPreviewWindowController.m
@@ -3,7 +3,7 @@
 //  DetoxInstruments
 //
 //  Created by Leo Natan (Wix) on 9/5/19.
-//  Copyright © 2019 Wix. All rights reserved.
+//  Copyright © 2017-2019 Wix. All rights reserved.
 //
 
 #import "DTXExpandedPreviewWindowController.h"
--- DetoxInstruments/DetoxInstruments/Views/DTXScrollForwardingContainerView.h
+++ DetoxInstruments/DetoxInstruments/Views/DTXScrollForwardingContainerView.h
@@ -3,7 +3,7 @@
 //  DetoxInstruments
 //
 //  Created by Leo Natan (Wix) on 9/4/19.
-//  Copyright © 2019 Wix. All rights reserved.
+//  Copyright © 2017-2019 Wix. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
