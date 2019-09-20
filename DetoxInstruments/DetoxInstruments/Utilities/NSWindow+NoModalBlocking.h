68af8b9f45debeb903b24a62a6f44273be4e55e4
Leo Natan <leo.natan@outlook.com>
2019-09-20 16:50:14 +0300

    Do not block application quit on internal modal windows
    References #54

---
+++ DetoxInstruments/DetoxInstruments/Utilities/NSWindow+NoModalBlocking.h
@@ -0,0 +1,17 @@
+//
+//  NSWindow+NoModalBlocking.h
+//  DetoxInstruments
+//
+//  Created by Leo Natan (Wix) on 9/20/19.
+//  Copyright © 2019 Wix. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSWindow (NoModalBlocking)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DetoxInstruments/DetoxInstruments/Utilities/NSWindow+NoModalBlocking.m
@@ -0,0 +1,43 @@
+//
+//  NSWindow+NoModalBlocking.m
+//  DetoxInstruments
+//
+//  Created by Leo Natan (Wix) on 9/20/19.
+//  Copyright © 2019 Wix. All rights reserved.
+//
+
+#import "NSWindow+NoModalBlocking.h"
+@import ObjectiveC;
+
+@implementation NSWindow (NoModalBlocking)
+
++ (void)load
+{
+	@autoreleasepool
+	{
+		SEL methodSel = NSSelectorFromString(@"_blocksActionWhenModal:");
+		Method m = class_getInstanceMethod(NSWindow.class, methodSel);
+		if(m == NULL)
+		{
+			return;
+		}
+		
+		BOOL (*orig)(id, SEL, SEL) = (void*)method_getImplementation(m);
+		method_setImplementation(m, imp_implementationWithBlock(^ (NSWindow* _self, SEL action) {
+			if(action != @selector(terminate:))
+			{
+				return orig(_self, methodSel, action);
+			}
+			
+			NSString* className = NSStringFromClass(_self.contentViewController.class);
+			if([className hasPrefix:@"DTX"] || [className hasPrefix:@"_DTX"])
+			{
+				return NO;
+			}
+			
+			return orig(_self, methodSel, action);
+		}));
+	}
+}
+
+@end
