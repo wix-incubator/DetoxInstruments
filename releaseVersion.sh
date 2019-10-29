89912c652c7e5ba01ad4de9c839b139df2480c96
Leo Natan <leo.natan@outlook.com>
2019-10-29 20:43:09 +0200

    Set Xcode 11.1 as the latest supported GM Xcode

--- releaseVersion.sh
+++ releaseVersion.sh
@@ -9,7 +9,7 @@
 #		brew install jq
 
 XCODEVERSION=$(xcodebuild -version | grep -oEi "([0-9]*\.[0-9]*)")
-XCODENEWESTSUPPORTED="11.0"
+XCODENEWESTSUPPORTED="11.1"
 if [ ${XCODEVERSION} != ${XCODENEWESTSUPPORTED} ] && [ "${XCODEVERSION}" = "`echo -e "${XCODEVERSION}\n${XCODENEWESTSUPPORTED}" | sort --version-sort -r | head -n1`" ]; then
   printf >&2 "\033[1;31mUnsupported Xcode, aborting\033[0m\n"
   exit 1;
