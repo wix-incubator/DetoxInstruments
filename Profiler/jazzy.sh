7f29a192c51fb766b4c26491e447a8cb62dcbedb
Leo Natan <LeoNatan@users.noreply.github.com>
2019-11-08 00:23:16 +0200

    Delete jazzy.sh

--- Profiler/jazzy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -e
-#--skip-undocumented 
-jazzy --objc --clean --author Wix --author_url https://github.com/wix --github_url https://github.com/wix/DetoxInstruments --module-version 1.6 --umbrella-header Profiler/DTXProfiler/DTXProfiler.h --framework-root Profiler/ --module DTXProfiler --theme fullwidth --readme README.md
