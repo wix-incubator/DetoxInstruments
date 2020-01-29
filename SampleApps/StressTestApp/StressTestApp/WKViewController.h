e5877c27a1f7c1d9a7908a6fb1549d68a38c13e0
Leo Natan <leo.natan@outlook.com>
2020-01-29 12:27:11 +0200

    Add WKWebView to example project

---
+++ SampleApps/StressTestApp/StressTestApp/WKViewController.h
@@ -0,0 +1,17 @@
+//
+//  WKViewController.h
+//  StressTestApp
+//
+//  Created by Leo Natan (Wix) on 1/28/20.
+//  Copyright © 2020 Wix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WKViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SampleApps/StressTestApp/StressTestApp/WKViewController.m
@@ -0,0 +1,28 @@
+//
+//  WKViewController.m
+//  StressTestApp
+//
+//  Created by Leo Natan (Wix) on 1/28/20.
+//  Copyright © 2020 Wix. All rights reserved.
+//
+
+#import "WKViewController.h"
+@import WebKit;
+
+@interface WKViewController ()
+{
+	IBOutlet WKWebView* _webView;
+}
+
+@end
+
+@implementation WKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+	[_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.google.com/search?tbm=isch&source=hp&biw=1445&bih=966&q=cute+labrador+puppy&oq=labrador+puppy&gs_l=img.12...0.0.1.179.0.0.0.0.0.0.0.0..0.0....0...1..64.img..0.0.0.kg6uB2QOnS0"]]];
+}
+
+@end
