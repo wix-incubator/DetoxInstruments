9def528da8739b8d06349aec43441530764cae5f
Leo Natan <leo.natan@outlook.com>
2020-11-12 12:02:32 +0200

    Disable RN flipper until bug is fixed

---
+++ Profiler/DTXProfiler/ReactNativeProfiling/DTXBrokenRNFlipper.h
@@ -0,0 +1,17 @@
+//
+//  DTXBrokenRNFlipper.h
+//  DTXProfiler
+//
+//  Created by Leo Natan on 11/12/20.
+//  Copyright © 2020 Wix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTXBrokenRNFlipper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
